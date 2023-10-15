0e7f5c8aba84dce092c6cc20838a78bc1a120d0f
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-10-15 20:08:57 +0530

    ....

--- Practice/bike.sql
+++ Practice/bike.sql
@@ -6,4 +6,10 @@ bike_name varchar(10) unique,
 bike_price int not null 	
 );
 
+insert into bike (bike_no,bike_name,bike_price)values
+(01,"bike-1",70000);
+insert into bike (bike_no,bike_name,bike_price)values
+(02,"bike-2",75000),
+(03,"bike-3",80000),
+(04,"bike-4",90000),
 select * from bike;
