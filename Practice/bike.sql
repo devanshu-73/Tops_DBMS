cc171d821691f765b8bde6b2b3934d2d97535994
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-10-16 16:15:37 +0530

    ....

--- Practice/bike.sql
+++ Practice/bike.sql
@@ -1,5 +1,6 @@
 -- DML insert,update,delete
 use vahicle;
+
 create table bike(
 bike_no int primary key,
 bike_name varchar(10) unique,
@@ -8,8 +9,16 @@ bike_price int not null
 
 insert into bike (bike_no,bike_name,bike_price)values
 (01,"bike-1",70000);
+
 insert into bike (bike_no,bike_name,bike_price)values
 (02,"bike-2",75000),
 (03,"bike-3",80000),
-(04,"bike-4",90000),
+(04,"bike-4",90000);
+
+
+
+-- order 
+-- where
+-- OR,AND
+-- in,between,like
 select * from bike;
