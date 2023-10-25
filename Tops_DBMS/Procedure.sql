dbabdc3377ab452c0c9787a3837f64a16b909588
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-10-25 17:17:43 +0530

    ...

---
+++ Tops_DBMS/Procedure.sql
@@ -0,0 +1,17 @@
+use finance;
+
+-- procedure(SQL Function)
+-- u can write any symbol for delimiter like ##,$$,**,&& ....
+delimiter ## 
+create procedure display()
+begin
+
+select * from student;
+select * from result;
+end ##
+
+call display();
+
+-- if u want to update your procedure then u have to drop your procedure first
+
+drop procedure display;
