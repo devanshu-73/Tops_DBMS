a8a12f087fa1c17c2b82c23297ffdd005d48ffef
devanshu-73 <94283941+devanshu-73@users.noreply.github.com>
2024-03-12 20:29:19 +0530

    Update aggregate-fun.md

---
+++ Practice/aggregate-fun.sql
@@ -0,0 +1,8 @@
+The most commonly used SQL aggregate functions are:
+
+MIN() - returns the smallest value within the selected column
+MAX() - returns the largest value within the selected column
+COUNT() - returns the number of rows in a set
+SUM() - returns the total sum of a numerical column
+AVG() - returns the average value of a numerical column
+Aggregate functions ignore null values (except for COUNT()).
